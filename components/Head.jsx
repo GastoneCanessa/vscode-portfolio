4b13bcddcd174d0386db487b36ac1c3925add37c
Gastone Canessa <34108337+GastoneCanessa@users.noreply.github.com>
2024-03-12 21:49:05 +0100

    wewe

--- components/Head.jsx
+++ components/Head.jsx
@@ -18,7 +18,7 @@ const CustomHead = ({ title }) => {
         content="Uno sviluppatore full-stack che realizza siti web che ti piacerebbe usare."
       />
       <meta property="og:image" content="/Screenshot.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
+      <meta property="og:url" content="https://gastonecanessaporfolio.vercel.app" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
